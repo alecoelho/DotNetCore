069a40be25e20355432f1ebc153160d5f5f59564
alecoelho <alecoelho2009@gmail.com>
2024-07-29 13:24:54 -0700

    Update InMemoryBus.cs

--- Eventos.IO/Eventos.IO.Infra.CrossCutting.Bus/InMemoryBus.cs
+++ Eventos.IO/Eventos.IO.Infra.CrossCutting.Bus/InMemoryBus.cs
@@ -6,34 +6,28 @@ using Eventos.IO.Domain.Core.Notifications;
 
 namespace Eventos.IO.Infra.CrossCutting.Bus
 {
-    public sealed class InMemoryBus : IBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
-        //Metodo de acesso ao conatainer de injecao de dependencia
-        //Injecao de dependencia para objetos genericos
-        //Settado no StarUp.cs
-        public static Func<IServiceProvider> ContainerAccessor { get; set; }
-        private static IServiceProvider Container => ContainerAccessor();
+        private readonly IMediator _mediator;
+        private readonly IEventStore _eventStore;
 
-        public void RaiseEvent<T>(T theEvent) where T : Event
+        public InMemoryBus(IEventStore eventStore, IMediator mediator)
         {
-            Publish(theEvent);
+            _eventStore = eventStore;
+            _mediator = mediator;
         }
 
-        public void SendCommand<T>(T theCommand) where T : Command
+        public async Task PublishEvent<T>(T @event) where T : Event
         {
-            Publish(theCommand);
+            if (!@event.MessageType.Equals("DomainNotification"))
+                _eventStore?.Save(@event);
+
+            await _mediator.Publish(@event);
         }
 
-        private static void Publish<T>(T message) where T : Message
+        public async Task<ValidationResult> SendCommand<T>(T command) where T : Command
         {
-            if (Container == null) return;
-
-            var obj = Container.GetService(message.MessageType.Equals("DomainNotification")
-                ? typeof(IDomainNotificationHandler<T>) //Somente DomainNotification
-                : typeof(IHandler<T>)); //Command ou Evento
-
-            //Decide qual Handle deve chamar
-            ((IHandler<T>)obj).Handle(message);
+            return await _mediator.Send(command);
         }
     }
 }
